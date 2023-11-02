0f1c944854318d7b079c109c26f1075f4a0302d4
nabeel <nabeelali720@gmail.com>
2023-11-02 16:36:57 +0500

    first

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import DealsOffer from './Components/DealsOffer'
+// import HotelCardDetails from './Components/HotelCardDetails'
+import Navigation from './Config/Navigation'
+// import FoodPreview from './Components/FoodPreview'
+// import Footer from './Components/Footer'
+
+function App() {
+  return (
+    <div>
+      <Navigation/>
+      {/* <FoodPreview/> */}
+
+      {/* <HotelCardDetails/> */}
+
+      {/* <DealsOffer/> */}
+
+    </div>
+  )
+}
+
+export default App
